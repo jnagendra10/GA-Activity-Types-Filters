2e3da602dabbd454c32eac11cce9acf155b234e2
jnagendra10 <jnagendra.sdce@gmail.com>
2026-08-13 13:18:16 +0530

    re-testing both pull-request and push filters

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -5,7 +5,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h4>{title}</h4>
+      <h5>{title}</h5>
       <p>{text}</p>
     </article>
   );
