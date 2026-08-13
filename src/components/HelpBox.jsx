e39c13c81c755fc08bdea03373967dcc4cdb475a
jnagendra10 <jnagendra.sdce@gmail.com>
2026-08-13 20:41:11 +0530

    testing activity types and filters

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -5,7 +5,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h6>{title}</h6>
+      <h7>{title}</h7>
       <p>{text}</p>
     </article>
   );
